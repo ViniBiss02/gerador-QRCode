e07b1bfab2d99dc44d59fef10ff86b77602389ac
ViniBiss02 <viniciusbiss02@gmail.com>
2026-02-14 20:09:14 -0300

    Initial commit

---
+++ src/main/java/com/example/gerador/QRCode/GeradorQrCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerador.QRCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorQrCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorQrCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerador/QRCode/GeradorQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerador.QRCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorQrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
